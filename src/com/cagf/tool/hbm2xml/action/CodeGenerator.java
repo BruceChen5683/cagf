70fdc76c6e5c5e9fb58f6c71ce20e049c926b46c
cjl <605385881@qq.com>
2019-04-02 07:43:20 +0800

    action xml

---
+++ src/com/cagf/tool/hbm2xml/action/CodeGenerator.java
@@ -0,0 +1,45 @@
+package com.cagf.tool.hbm2xml.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CodeGenerator
+{
+	private static String outputPath;
+
+	private static String fileName;
+
+	private static String projectDirectory;
+
+	private static List<String> hbmFiles = new ArrayList<String>();
+
+	public static void main(String[] args)
+	{
+		System.out.println("generating Spring Action configs...");
+
+		for (String temp : args)
+		{
+			if (temp.startsWith("--output="))
+			{
+				outputPath = temp.substring(9); // 设定xml文件输出目录
+			}
+			else if (temp.startsWith("--filename="))
+			{
+				fileName = temp.substring(11); // 设定配置文件名
+			}
+			else if (temp.startsWith("--projectDirectory="))
+			{
+				projectDirectory = temp.substring(19);
+			}
+			else
+			{
+				hbmFiles.add(temp); // 将hbm全路径加到集合中
+			}
+		}
+
+		Helper.getInstance().generateActionXML(hbmFiles, outputPath, fileName,
+				projectDirectory);
+
+		System.out.println("finishing Spring Action configs...");
+	}
+}
